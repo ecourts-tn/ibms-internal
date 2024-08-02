e312a29c9bf3c5a5c91184265ca0e36f1e419795
ecourts-tn <deenadayalan.mhc@gmail.com>
2024-08-02 17:46:57 +0530

    02Aug2024

--- src/api.js
+++ src/api.js
@@ -1,26 +1,49 @@
 import axios from "axios";
-// import { ACCESS_TOKEN } from "./constants";
+import { ACCESS_TOKEN, REFRESH_TOKEN } from "./constants";
 
-//const apiUrl = "http://192.168.100.135:8000/";
-
-const apiUrl = "http://127.0.0.1:8000/"
+const apiUrl = "http://117.193.76.243:8088/"
 
 const api = axios.create({
-//   baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
     baseURL: apiUrl
 });
 
-// api.interceptors.request.use(
-//   (config) => {
-//     const token = localStorage.getItem(ACCESS_TOKEN);
-//     if (token) {
-//       config.headers.Authorization = `Bearer ${token}`;
-//     }
-//     return config;
-//   },
-//   (error) => {
-//     return Promise.reject(error);
-//   }
-// );
+let refresh = false;
 
-export default api;
+api.interceptors.request.use(
+  (config) => {
+        const token = localStorage.getItem(ACCESS_TOKEN);
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+        return config;
+  },
+  (error) => {
+        return Promise.reject(error);
+  }
+);
+
+api.interceptors.response.use(resp => resp, async error => {  
+    if (error.config && error.config.skipInterceptor) {
+        return Promise.reject(error);
+    }
+    if (error.response.status === 401 && !refresh) {     
+        refresh = true;
+        const response = await api.post('api/auth/token/refresh/', {      
+        refresh:localStorage.getItem(REFRESH_TOKEN)
+        },{ 
+            headers: {'Content-Type': 'application/json'}
+        },{
+            withCredentials: true
+        });    
+        if (response.status === 200) {
+            api.defaults.headers.common['Authorization'] = `Bearer ${response.data['access']}`;       
+            localStorage.setItem(ACCESS_TOKEN, response.data.access);       
+            localStorage.setItem(REFRESH_TOKEN, response.data.refresh);       
+            return axios(error.config);
+        }  
+    }
+    refresh = false;
+    return error;
+});
+
+export default api
